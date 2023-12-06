40096e3864be155d52eaa95278d45f3b54ccdd20
qyzaikber <143592832+qyzaikber@users.noreply.github.com>
2023-12-06 14:35:42 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import "./globals.css";
 import { Navigation } from "./ui/Navigation";
 
 export const metadata: Metadata = {
-  title: "IMGPrompt - Stable Diffusion 和 Midjourney 的图像提示词生成工具",
+  title: "澎蒙AI - Stable Diffusion 和 Midjourney 的图像提示词生成工具",
   description:
-    "IMGPrompt 是一个直观的图像提示词生成工具，可以方便地在 Stable Diffusion 和 Midjourney 的流程中使用，使图像提示词的创建变得简单而有效，轻松激发创意并获得更好的图片结果。通过 IMGPrompt，你可以将自己的创意想法转化为视觉现实。",
+    "澎蒙AI的图像提示词生成工具，可以方便地在 Stable Diffusion 和 Midjourney 的流程中使用，使图像提示词的创建变得简单而有效，轻松激发创意并获得更好的图片结果。通过 IMGPrompt，你可以将自己的创意想法转化为视觉现实。",
 };
 
 export default function RootLayout({
